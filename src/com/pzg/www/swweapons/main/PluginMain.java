d6425372ee8ad9ea692d52269b0e883aece1c9c0
TJPlaysNow <pannebakertj@gmail.com>
2017-05-15 18:28:00 -0500

    The start
    First update to StarWarsWeapons
    
    New things:
    - Created PluginMain
    
    Things to come:
    - Everything else?

---
+++ src/com/pzg/www/swweapons/main/PluginMain.java
@@ -0,0 +1,16 @@
+package com.pzg.www.swweapons.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
